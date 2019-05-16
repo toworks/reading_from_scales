882d7fe58b5b810e8fa466c72eb645c73ec1b6d7
Arhipov, Sergey A <sergey.arhipov@arcelormittal.com>
2019-05-16 07:35:24 +0000

    remove merge. merge inside in view trigger

--- _sql.pm
+++ _sql.pm
@@ -14,20 +14,8 @@ package _sql;{
 
     $self->conn() if ( $self->{sql}->{error} == 1 or ! $self->{sql}->{dbh}->ping );
 
-	$query  = "SET TRANSACTION ISOLATION LEVEL SERIALIZABLE ";
-	$query .= "MERGE [$self->{sql}->{database}]..$self->{sql}->{table} AS trg ";
-    $query .= "USING (SELECT ? ID_Scales, ? DT, ? WeightStabilized_1, ? Weight_platform_1) AS src ";
-    $query .= "    ON src.ID_Scales = trg.ID_Scales ";
-    $query .= "WHEN MATCHED THEN UPDATE ";
-    $query .= "    SET DT                 = src.DT, ";
-	$query .= "    	   WeightStabilized_1 = src.WeightStabilized_1, ";
-	$query .= "    	   Weight_platform_1  = src.Weight_platform_1 ";
-    $query .= "WHEN NOT MATCHED THEN  ";
-    $query .= "    INSERT (ID_Scales, DT, WeightStabilized_1, Weight_platform_1)  ";
-    $query .= "    VALUES (?, ?, ?, ?); ";
-
-	# transfer 4 parametera in query as 8
-	push @values, @values;
+    $query  = "INSERT [$self->{sql}->{database}]..$self->{sql}->{table} (ID_Scales, DT, WeightStabilized_1, Weight_platform_1) ";
+    $query .= "VALUES (?, ?, ?, ?) ";
 
 	$self->{log}->save('d', "values: ". Dumper(@values)) if $self->{sql}->{'DEBUG'};
 	$self->{log}->save('d', "query: ". $query) if $self->{sql}->{'DEBUG'};
