a73af26b5d2caa6c71487bb4ef1043287f20b6b6
saarhipov <sergey.arhipov@arcelormittal.com>
2021-02-16 09:08:29 +0200

    perl x32 for work serial port

--- service/service_install.cmd
+++ service/service_install.cmd
@@ -3,7 +3,7 @@ CLS
 
 set NSSM=C:\bin\language\perl\perl\bin\nssm_64.exe
 set SERVICE_NAME=reading_from_scales.10.27.232.62
-set APP_PATH=C:\bin\language\perl\perl\bin\perl.exe
+set APP_PATH=C:\bin\language\perl32\perl\bin\perl.exe
 rem set APP_DIR=C:\bin\daemon\%SERVICE_NAME%
 set APP_DIR=C:\bin\daemon\reading_from_scales
 set APP_PARAMETERS="%SERVICE_NAME%.pl"
