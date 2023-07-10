c02ae72988f9b41b2a0e70f36267dbf1a87e25fa
Arhipov, Sergey A <sergey.arhipov@arcelormittal.com>
2023-07-10 14:52:06 +0300

    service/install-10.27.232.62.cmd: update description

--- service/install-10.27.232.62.cmd
+++ service/install-10.27.232.62.cmd
@@ -7,7 +7,7 @@ set APP_PATH=C:\bin\language\perl32\perl\bin\perl.exe
 set APP_DIR=C:\bin\daemon\reading_from_scales
 set APP_PARAMETERS="%SERVICE_NAME%.pl"
 set APP_DISPLAYNAME="ArcelorMittal.%SERVICE_NAME%"
-set APP_DESCRIPTION="read data from 10.27.226.87"
+set APP_DESCRIPTION="read data from 10.27.232.62"
 set APP_ROTATEBYTES=26214400
 set APP_ENVIRONMENTEXTRA="PATH=C:\bin\language\perl\c\bin;C:\bin\oracle\instantclient_19_10;%PATH%"
 set APP_ENVIRONMENTEXTRA_2="ORACLE_HOME=C:\bin\oracle\instantclient_19_10"
