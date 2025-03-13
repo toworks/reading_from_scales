f047770be9edfdd30c207b8d1fd2a9ea6fc8aea6
Arhipov <sergey.arhipov@arcelormittal.com>
2025-03-13 16:23:31 +0200

    src/scales/network.go: test write command and read replay

--- src/scales/network.go
+++ src/scales/network.go
@@ -2,7 +2,6 @@ package scales
 
 import (
 	"fmt"
-	"os"
     "time"
 	"strings"
 	"net"
@@ -31,7 +30,7 @@ func (c *Config) Connect() {
   t1 := time.Now()
   if c.Network.Protocol == "tcp" {
 	c.connTCP, err = net.DialTCP(c.Network.Protocol, nil, resolveTCP)
-	c.ch_message <- fmt.Sprintf("e|:|%s:  connTCP: %#v", mod_name, c.connTCP)
+	c.ch_message <- fmt.Sprintf("e|:|%s: connTCP: %#v", mod_name, c.connTCP)
   } else if c.Network.Protocol == "udp" {
 	c.connUDP, err = net.DialUDP(c.Network.Protocol, nil, resolveUDP)
   }
@@ -44,9 +43,10 @@ func (c *Config) Connect() {
   }
 }
 
-func (c *Config) Read() {
+func (c *Config) Write() {
   var err error
-	cycle := 1000
+  connection_string := c.Network.Host+":"+fmt.Sprintf("%d", c.Network.Port)
+  cycle := 1000
 
 	timer := time.NewTicker(time.Duration(cycle) * time.Millisecond)
 	for _ = range timer.C {
@@ -66,24 +66,51 @@ fmt.Printf(">>>>>>>>>>>>>>>>\n")
 
 						_, err = c.connTCP.Write([]byte(c.Command))
 						if err != nil {
-							println("Write to server failed:", err.Error())
-							os.Exit(1)
+							c.ch_message <- fmt.Sprintf("e|:|%s: write failed: %s  error: %s", mod_name, connection_string, err.Error())
 						}
 
-						println("write to server = ", c.Command)
+	if DEBUG.enable {
+		c.ch_message <- fmt.Sprintf("d|:|%s: send command: %s", mod_name, c.Command)
+	}
 
+c.Read()
+/*
 						reply := make([]byte, 1024)
 
 						_, err = c.connTCP.Read(reply)
 						if err != nil {
 							println("Write to server failed:", err.Error())
-							os.Exit(1)
+							//os.Exit(1)
 						}
 
 						println("reply from server=", string(reply))
-
+*/
 						//c.connTCP.Close()
 	
 	}
+}
+
+func (c *Config) Read() {
+  var err error
+  connection_string := c.Network.Host+":"+fmt.Sprintf("%d", c.Network.Port)
+
+  reply := make([]byte, 1024)
+
+  t1 := time.Now()
+  _, err = c.connTCP.Read(reply)
+  t2 := time.Now()
+  if err != nil {
+	c.ch_message <- fmt.Sprintf("e|:|%s: read failed: %s  error: %s", mod_name, connection_string, err.Error())
+  }
+  if DEBUG.enable {
+	c.ch_message <- fmt.Sprintf("d|:|%s: raw data: %s", mod_name, string(reply))
+	c.ch_message <- fmt.Sprintf("d|:|%s: read time: %s", mod_name, t2.Sub(t1))
+  }
+  
+  c.Type = strings.ToLower(c.Type)
+
+  if c.Type == "bullat" {
+	c.ProcessingBullat(string(reply))
+  }
 }
 
