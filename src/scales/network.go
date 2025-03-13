54f2e27226973a2e067e27ee65012ea035b008ce
Arhipov <sergey.arhipov@arcelormittal.com>
2025-03-13 15:07:53 +0200

    add simple network connection

---
+++ src/scales/network.go
@@ -0,0 +1,89 @@
+package scales
+
+import (
+	"fmt"
+	"os"
+    "time"
+	"strings"
+	"net"
+)
+
+
+func (c *Config) Connect() {
+	var err error
+	var resolveTCP *net.TCPAddr
+	var resolveUDP *net.UDPAddr
+
+  connection_string := c.Network.Host+":"+fmt.Sprintf("%d", c.Network.Port)
+  c.Network.Protocol = strings.ToLower(c.Network.Protocol)
+
+  if c.Network.Protocol == "tcp" {
+	resolveTCP, err = net.ResolveTCPAddr(c.Network.Protocol, connection_string)
+  } else if c.Network.Protocol == "udp" {
+	resolveUDP, err = net.ResolveUDPAddr(c.Network.Protocol, connection_string)
+  } else {
+	err = fmt.Errorf("unsupported protocol: ", c.Network.Protocol)
+  }
+  if err != nil {
+	c.ch_message <- fmt.Sprintf("e|:|%s: resolve failed: %s  error: %s", mod_name, connection_string, err.Error())
+  }
+
+  t1 := time.Now()
+  if c.Network.Protocol == "tcp" {
+	c.connTCP, err = net.DialTCP(c.Network.Protocol, nil, resolveTCP)
+	c.ch_message <- fmt.Sprintf("e|:|%s:  connTCP: %#v", mod_name, c.connTCP)
+  } else if c.Network.Protocol == "udp" {
+	c.connUDP, err = net.DialUDP(c.Network.Protocol, nil, resolveUDP)
+  }
+  t2 := time.Now()
+  if err != nil {
+	c.ch_message <- fmt.Sprintf("e|:|%s: dial failed: %s  error: %s", mod_name, connection_string, err.Error())
+  }
+  if DEBUG.enable {
+	c.ch_message <- fmt.Sprintf("d|:|%s: connect time: %s", mod_name, t2.Sub(t1))
+  }
+}
+
+func (c *Config) Read() {
+  var err error
+	cycle := 1000
+
+	timer := time.NewTicker(time.Duration(cycle) * time.Millisecond)
+	for _ = range timer.C {
+fmt.Printf(">>>>>>>>>>>>>>>>\n")
+//						servAddr := "10.27.226.67:25687"
+//						tcpAddr, err := net.ResolveTCPAddr("tcp", servAddr)
+//						if err != nil {
+//							println("ResolveTCPAddr failed:", err.Error())
+//							os.Exit(1)
+//						}
+
+//						conn, err := net.DialTCP("tcp", nil, tcpAddr)
+//						if err != nil {
+//							println("Dial failed:", err.Error())
+//							os.Exit(1)
+//						}
+
+						_, err = c.connTCP.Write([]byte(c.Command))
+						if err != nil {
+							println("Write to server failed:", err.Error())
+							os.Exit(1)
+						}
+
+						println("write to server = ", c.Command)
+
+						reply := make([]byte, 1024)
+
+						_, err = c.connTCP.Read(reply)
+						if err != nil {
+							println("Write to server failed:", err.Error())
+							os.Exit(1)
+						}
+
+						println("reply from server=", string(reply))
+
+						//c.connTCP.Close()
+	
+	}
+}
+
