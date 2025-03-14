ca7b459f7590276657a159af58c4fce7c4769186
Arhipov <sergey.arhipov@arcelormittal.com>
2025-03-14 12:00:51 +0200

    src/scales/network.go: Write() -> Network_send() and Read() -> Network_receive()

--- src/scales/network.go
+++ src/scales/network.go
@@ -8,7 +8,7 @@ import (
 )
 
 
-func (c *Config) Connect() {
+func (c *Config) Network_connect() error {
 	var err error
 	var resolveTCP *net.TCPAddr
 	var resolveUDP *net.UDPAddr
@@ -25,6 +25,7 @@ func (c *Config) Connect() {
   }
   if err != nil {
 	c.ch_message <- fmt.Sprintf("e|:|%s: resolve failed: %s  error: %s", mod_name, connection_string, err.Error())
+	return err
   }
 
   t1 := time.Now()
@@ -37,60 +38,37 @@ func (c *Config) Connect() {
   t2 := time.Now()
   if err != nil {
 	c.ch_message <- fmt.Sprintf("e|:|%s: dial failed: %s  error: %s", mod_name, connection_string, err.Error())
+	return err
   }
   if DEBUG.enable {
 	c.ch_message <- fmt.Sprintf("d|:|%s: connect time: %s", mod_name, t2.Sub(t1))
   }
+  return nil
 }
 
-func (c *Config) Write() {
+//  переделать убрать/изменить цикл запроса
+func (c *Config) Network_send() {
   var err error
   connection_string := c.Network.Host+":"+fmt.Sprintf("%d", c.Network.Port)
   cycle := 1000
 
 	timer := time.NewTicker(time.Duration(cycle) * time.Millisecond)
 	for _ = range timer.C {
-fmt.Printf(">>>>>>>>>>>>>>>>\n")
-//						servAddr := "10.27.226.67:25687"
-//						tcpAddr, err := net.ResolveTCPAddr("tcp", servAddr)
-//						if err != nil {
-//							println("ResolveTCPAddr failed:", err.Error())
-//							os.Exit(1)
-//						}
-
-//						conn, err := net.DialTCP("tcp", nil, tcpAddr)
-//						if err != nil {
-//							println("Dial failed:", err.Error())
-//							os.Exit(1)
-//						}
-
-						_, err = c.connTCP.Write([]byte(c.Command))
-						if err != nil {
-							c.ch_message <- fmt.Sprintf("e|:|%s: write failed: %s  error: %s", mod_name, connection_string, err.Error())
-						}
-
-	if DEBUG.enable {
-		c.ch_message <- fmt.Sprintf("d|:|%s: send command: %s", mod_name, c.Command)
-	}
-
-c.Read()
-/*
-						reply := make([]byte, 1024)
-
-						_, err = c.connTCP.Read(reply)
-						if err != nil {
-							println("Write to server failed:", err.Error())
-							//os.Exit(1)
-						}
-
-						println("reply from server=", string(reply))
-*/
-						//c.connTCP.Close()
-	
+		t1 := time.Now()
+		_, err = c.connTCP.Write([]byte(c.Command))
+		t2 := time.Now()
+		if err != nil {
+			c.ch_message <- fmt.Sprintf("e|:|%s: send failed: %s  error: %s", mod_name, connection_string, err.Error())
+		}
+		if DEBUG.enable {
+			c.ch_message <- fmt.Sprintf("d|:|%s: send command: %s", mod_name, c.Command)
+			c.ch_message <- fmt.Sprintf("d|:|%s: send command time: %s", mod_name, t2.Sub(t1))
+		}
+		c.Network_receive()
 	}
 }
 
-func (c *Config) Read() {
+func (c *Config) Network_receive() {
   var err error
   connection_string := c.Network.Host+":"+fmt.Sprintf("%d", c.Network.Port)
 
@@ -100,11 +78,11 @@ func (c *Config) Read() {
   _, err = c.connTCP.Read(reply)
   t2 := time.Now()
   if err != nil {
-	c.ch_message <- fmt.Sprintf("e|:|%s: read failed: %s  error: %s", mod_name, connection_string, err.Error())
+	c.ch_message <- fmt.Sprintf("e|:|%s: receive failed: %s  error: %s", mod_name, connection_string, err.Error())
   }
   if DEBUG.enable {
-	c.ch_message <- fmt.Sprintf("d|:|%s: raw data: %s", mod_name, string(reply))
-	c.ch_message <- fmt.Sprintf("d|:|%s: read time: %s", mod_name, t2.Sub(t1))
+	c.ch_message <- fmt.Sprintf("d|:|%s: receive raw data: %s", mod_name, string(reply))
+	c.ch_message <- fmt.Sprintf("d|:|%s: receive time: %s", mod_name, t2.Sub(t1))
   }
   
   c.Type = strings.ToLower(c.Type)
@@ -113,4 +91,3 @@ func (c *Config) Read() {
 	c.ProcessingBullat(string(reply))
   }
 }
-
