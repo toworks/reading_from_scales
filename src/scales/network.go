6540e3bf71c751b50f0b02c7979b6219ae601bd2
Arhipov <sergey.arhipov@arcelormittal.com>
2025-04-04 11:59:01 +0300

    src/scales/network.go: added polling cycle setting from config

--- src/scales/network.go
+++ src/scales/network.go
@@ -50,23 +50,28 @@ func (c *Config) Network_connect() error {
 //  переделать убрать/изменить цикл запроса
 func (c *Config) Network_send() {
   var err error
-  connection_string := c.Network.Host+":"+fmt.Sprintf("%d", c.Network.Port)
-  cycle := 1000
 
-	timer := time.NewTicker(time.Duration(cycle) * time.Millisecond)
-	for _ = range timer.C {
-		t1 := time.Now()
-		_, err = c.connTCP.Write([]byte(c.Command))
-		t2 := time.Now()
-		if err != nil {
-			c.ch_message <- fmt.Sprintf("e|:|%s: send failed: %s  error: %s", mod_name, connection_string, err.Error())
-		}
-		if DEBUG.enable {
-			c.ch_message <- fmt.Sprintf("d|:|%s: send command: %s", mod_name, c.Command)
-			c.ch_message <- fmt.Sprintf("d|:|%s: send command time: %s", mod_name, t2.Sub(t1))
-		}
-		c.Network_receive()
-	}
+  connection_string := c.Network.Host+":"+fmt.Sprintf("%d", c.Network.Port)
+
+  if c.Read_cycle > 1000 * 120 || c.Read_cycle <= 0 {
+    c.Read_cycle := 1000
+  }
+
+  timer := time.NewTicker(time.Duration(cycle) * time.Millisecond)
+
+  for _ = range timer.C {
+      t1 := time.Now()
+      _, err = c.connTCP.Write([]byte(c.Command))
+      t2 := time.Now()
+      if err != nil {
+          c.ch_message <- fmt.Sprintf("e|:|%s: send failed: %s  error: %s", mod_name, connection_string, err.Error())
+      }
+      if DEBUG.enable {
+          c.ch_message <- fmt.Sprintf("d|:|%s: send command: %s", mod_name, c.Command)
+          c.ch_message <- fmt.Sprintf("d|:|%s: send command time: %s", mod_name, t2.Sub(t1))
+      }
+      c.Network_receive()
+  }
 }
 
 func (c *Config) Network_receive() {
