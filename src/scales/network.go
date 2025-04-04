4ca29e7e932cc5b084582e98965f09b28519c391
Arhipov <sergey.arhipov@arcelormittal.com>
2025-04-04 12:00:04 +0300

    src/scales/network.go: replace tab to space

--- src/scales/network.go
+++ src/scales/network.go
@@ -1,48 +1,48 @@
 package scales
 
 import (
-	"fmt"
+    "fmt"
     "time"
-	"strings"
-	"regexp"
-	"net"
+    "strings"
+    "regexp"
+    "net"
 )
 
 
 func (c *Config) Network_connect() error {
-	var err error
-	var resolveTCP *net.TCPAddr
-	var resolveUDP *net.UDPAddr
+    var err error
+    var resolveTCP *net.TCPAddr
+    var resolveUDP *net.UDPAddr
 
   connection_string := c.Network.Host+":"+fmt.Sprintf("%d", c.Network.Port)
   c.Network.Protocol = strings.ToLower(c.Network.Protocol)
 
   if c.Network.Protocol == "tcp" {
-	resolveTCP, err = net.ResolveTCPAddr(c.Network.Protocol, connection_string)
+    resolveTCP, err = net.ResolveTCPAddr(c.Network.Protocol, connection_string)
   } else if c.Network.Protocol == "udp" {
-	resolveUDP, err = net.ResolveUDPAddr(c.Network.Protocol, connection_string)
+    resolveUDP, err = net.ResolveUDPAddr(c.Network.Protocol, connection_string)
   } else {
-	err = fmt.Errorf("unsupported protocol: ", c.Network.Protocol)
+    err = fmt.Errorf("unsupported protocol: ", c.Network.Protocol)
   }
   if err != nil {
-	c.ch_message <- fmt.Sprintf("e|:|%s: resolve failed: %s  error: %s", mod_name, connection_string, err.Error())
-	return err
+    c.ch_message <- fmt.Sprintf("e|:|%s: resolve failed: %s  error: %s", mod_name, connection_string, err.Error())
+    return err
   }
 
   t1 := time.Now()
   if c.Network.Protocol == "tcp" {
-	c.connTCP, err = net.DialTCP(c.Network.Protocol, nil, resolveTCP)
-	c.ch_message <- fmt.Sprintf("e|:|%s: connTCP: %#v", mod_name, c.connTCP)
+    c.connTCP, err = net.DialTCP(c.Network.Protocol, nil, resolveTCP)
+    c.ch_message <- fmt.Sprintf("e|:|%s: connTCP: %#v", mod_name, c.connTCP)
   } else if c.Network.Protocol == "udp" {
-	c.connUDP, err = net.DialUDP(c.Network.Protocol, nil, resolveUDP)
+    c.connUDP, err = net.DialUDP(c.Network.Protocol, nil, resolveUDP)
   }
   t2 := time.Now()
   if err != nil {
-	c.ch_message <- fmt.Sprintf("e|:|%s: dial failed: %s  error: %s", mod_name, connection_string, err.Error())
-	return err
+    c.ch_message <- fmt.Sprintf("e|:|%s: dial failed: %s  error: %s", mod_name, connection_string, err.Error())
+    return err
   }
   if DEBUG.enable {
-	c.ch_message <- fmt.Sprintf("d|:|%s: connect time: %s", mod_name, t2.Sub(t1))
+    c.ch_message <- fmt.Sprintf("d|:|%s: connect time: %s", mod_name, t2.Sub(t1))
   }
   return nil
 }
@@ -84,16 +84,16 @@ func (c *Config) Network_receive() {
   _, err = c.connTCP.Read(reply)
   t2 := time.Now()
   if err != nil {
-	c.ch_message <- fmt.Sprintf("e|:|%s: receive failed: %s  error: %s", mod_name, connection_string, err.Error())
+    c.ch_message <- fmt.Sprintf("e|:|%s: receive failed: %s  error: %s", mod_name, connection_string, err.Error())
   }
   if DEBUG.enable {
-	c.ch_message <- fmt.Sprintf("d|:|%s: receive raw data: %s", mod_name, string(reply))
-	c.ch_message <- fmt.Sprintf("d|:|%s: receive time: %s", mod_name, t2.Sub(t1))
+    c.ch_message <- fmt.Sprintf("d|:|%s: receive raw data: %s", mod_name, string(reply))
+    c.ch_message <- fmt.Sprintf("d|:|%s: receive time: %s", mod_name, t2.Sub(t1))
   }
   
   c.Type = strings.ToLower(c.Type)
 
   if c.Type == "systec" || regexp.MustCompile(`(?is)^b[uy].*at`).MatchString(c.Type) {
-	c.Processing_systec(string(reply))
+    c.Processing_systec(string(reply))
   }
 }
