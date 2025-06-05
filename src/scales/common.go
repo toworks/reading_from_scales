924ec85a942b5697575e7606a8543415c3b44b28
Arhipov <sergey.arhipov@arcelormittal.com>
2025-06-05 15:55:53 +0300

    src/scales/common.go: new handle lost connection

--- src/scales/common.go
+++ src/scales/common.go
@@ -7,24 +7,27 @@ import (
   "time"
   "regexp"
 
+
   "reading_from_scales/src/config"
   db "reading_from_scales/src/database"
 )
 
 type Config struct {
   *config.Scale
-  ch_message chan string
-  _connect net.Conn
+  ch_message                chan string
+  _connect                  net.Conn
   network_connection_string string
-  ch_db_message chan db.Kep_analytics_weight
+  ch_db_message             chan db.Kep_analytics_weight
 }
 
 type _debug struct {
   enable bool
-  level string
+  level  string
 }
 
 const TimeFormat string = "2006-01-02 15:04:05.000"
+/* set timeout read/write 10 sec */
+const read_write_timeout = 10000
 
 var (
   DEBUG = &_debug{}
@@ -51,29 +54,37 @@ func New(c *config.Scale, e bool, lv string, ch_message chan string, ch_db_messa
 }
 
 func (c *Config) Run() {
+  if regexp.MustCompile(`(?is)^network$`).MatchString(c.Connection) {
+	go func () {
+			err := fmt.Errorf("empty")
 
-  go func () {
-	err := fmt.Errorf("empty")
+			if c.Command != "" {
+				if c.Read_cycle > 1000 * 120 || c.Read_cycle <= 0 {
+					c.Read_cycle = 1000
+				}
 
-	if c.Read_cycle > 1000 * 120 || c.Read_cycle <= 0 {
-		c.Read_cycle = 1000
-	}
-
-	timer := time.NewTicker(time.Duration(c.Read_cycle) * time.Millisecond)
-
-	for _ = range timer.C {
-		if regexp.MustCompile(`(?is)^network$`).MatchString(c.Connection) {
-			if err != nil {
-				err = c.Network_connect();
-				continue
+				timer := time.NewTicker(time.Duration(c.Read_cycle) * time.Millisecond)
+				for _ = range timer.C {
+					if err != nil {
+						err = c.Network_connect()
+					}
+					err = c.Network_send()
+					err = c.Network_receive()
+				}
+			} else {
+				for {
+					if err != nil {
+						err = c.Network_connect()
+						if err != nil {
+							time.Sleep(time.Second * 2)
+							continue
+						}
+					}
+					err = c.Network_receive()
+				}
 			}
-		    if c.Command != "" {
-				err = c.Network_send()
-			}
-            err = c.Network_receive()
-		} else {
-			c.ch_message <- fmt.Sprintf("w|:|%s: connection type: '%s' not supported", mod_name, c.Connection)
-		}
-    }
-  }()
+	}()
+  } else {
+	c.ch_message <- fmt.Sprintf("w|:|%s: connection type: '%s' not supported", mod_name, c.Connection)
+  }
 }
