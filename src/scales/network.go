cbadc65c9120933baa53c7d65b5b25831aa2a195
Arhipov <sergey.arhipov@arcelormittal.com>
2025-06-05 15:59:17 +0300

    src/scales/network.go: split read/write deadline

--- src/scales/network.go
+++ src/scales/network.go
@@ -18,13 +18,11 @@ func (c *Config) Network_connect() error {
 
   t1 := time.Now()
   c._connect, err = net.Dial(c.Network.Protocol, c.network_connection_string)
-  t2 := time.Now()
   if err != nil {
     c.ch_message <- fmt.Sprintf("e|:|%s: dial failed: %s  error: %s", mod_name, c.network_connection_string, err.Error())
     return err
   }
-  /* set timeout read/write */
-  c._connect.SetDeadline(time.Now().Add(time.Second * 10))
+  t2 := time.Now()
 
   c.ch_message <- fmt.Sprintf("i|:|%s: connect: %s  time: %s", mod_name, c.network_connection_string, t2.Sub(t1))
   return nil
@@ -37,13 +35,15 @@ func (c *Config) Network_disconnect() error {
 func (c *Config) Network_send() error {
   var err error
 
+  c._connect.SetWriteDeadline(time.Now().Add(time.Millisecond * read_write_timeout))
+
   t1 := time.Now()
   _, err = c._connect.Write([]byte(c.Command))
-  t2 := time.Now()
   if err != nil {
         c.ch_message <- fmt.Sprintf("e|:|%s: send failed: %s  error: %s", mod_name, c.network_connection_string, err.Error())
         return err
   }
+  t2 := time.Now()
   if DEBUG.enable {
         c.ch_message <- fmt.Sprintf("d|:|%s: host: %s  time: %s  send command: %s", mod_name, c.network_connection_string, t2.Sub(t1), c.Command)
   }
@@ -52,28 +52,29 @@ func (c *Config) Network_send() error {
 }
 
 func (c *Config) Network_receive() error {
-  var err error
-/*
-  connection_string := c.Network.Host+":"+fmt.Sprintf("%d", c.Network.Port)
-*/
+  buf := make([]byte, 1024)
 
-  reply := make([]byte, 1024)
+  c._connect.SetReadDeadline(time.Now().Add(time.Millisecond * read_write_timeout))
 
   t1 := time.Now()
-  _, err = c._connect.Read(reply)
-  t2 := time.Now()
+  n, err := c._connect.Read(buf)
   if err != nil {
     c.ch_message <- fmt.Sprintf("e|:|%s: receive failed: %s  error: %s", mod_name, c.network_connection_string, err.Error())
 	return err
   }
+  t2 := time.Now()
   if DEBUG.enable {
-    c.ch_message <- fmt.Sprintf("d|:|%s: host: %s  time: %s  receive raw data: %s", mod_name, c.network_connection_string, t2.Sub(t1), string(reply))
+    c.ch_message <- fmt.Sprintf("d|:|%s: host: %s  time: %s  receive raw count: %d  data: %s", mod_name, c.network_connection_string, t2.Sub(t1), n, string(buf))
+  }
+
+  if n == 0 {
+	return nil
   }
 
   c.Type = strings.ToLower(c.Type)
 
   if c.Type == "systec" || regexp.MustCompile(`(?is)^b[uy].*at`).MatchString(c.Type) {
-    c.Processing_systec(string(reply))
+    c.Processing_systec(string(buf))
   }
 
   return nil
